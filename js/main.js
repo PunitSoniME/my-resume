1241856a070112d7f2a01a04f778c15b1e5cd4fb
punit <punit.soni33@gmail.com>
2021-02-14 19:29:26 +0530

    - Portfolio Updated

--- js/main.js
+++ js/main.js
@@ -1,59 +1,131 @@
+/*==================== SHOW MENU ====================*/
 const showMenu = (toggleId, navId) => {
-    const toggle = document.getElementById(toggleId);
-    nav = document.getElementById(navId);
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
 
+    // Validate that variables exist
     if (toggle && nav) {
         toggle.addEventListener('click', () => {
-            nav.classList.toggle('show');
-        });
+            // We add the show-menu class to the div tag with the nav__menu class
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMenu('nav-toggle', 'nav-menu');
+
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            if (document.querySelector('.nav__menu a[href*=' + sectionId + ']')) {
+                document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+            }
+        } else {
+            if (document.querySelector('.nav__menu a[href*=' + sectionId + ']')) {
+                document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+            }
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive);
+
+
+/*==================== SHOW SCROLL TOP ====================*/
+function scrollTop() {
+    const scrollTop = document.getElementById('scroll-top');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if (this.scrollY >= 200) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop);
+
+
+/*==================== DARK LIGHT THEME ====================*/
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bx-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+
+
+function scaleCV() {
+    document.body.classList.add("scale-cv");
+}
+
+
+function removeScale() {
+    document.body.classList.remove("scale-cv");
+}
+
+let areaCv = document.getElementById("area-cv");
+
+let resumeButton = document.getElementById("resume-button");
+
+let opt = {
+    margin: 0,
+    filename: 'punit-resume.pdf',
+    image: { type: 'jpeg', quality: 0.80 },
+    html2canvas: { scale: 4 },
+    jsPDF: { format: 'a4', orientation: 'portrait' },
+    enableLinks: true,
+    pagebreak: {
+        before: ['#projects', '#skills']
     }
 }
 
-showMenu('nav-toggle', 'nav-menu');
-
-// Changing Primary Color
-let root = document.documentElement;
-
-function colorChange(e) {
-    root.style.setProperty('--first-color', e.value);
-}
-// Changing Primary Color
-
-const navLink = document.querySelectorAll('.nav_link');
-
-function linkAction() {
-    navLink.forEach(n => n.classList.remove('active'));
-    this.classList.add('active');
-
-    const navMenu = document.getElementById('nav-menu');
-    navMenu.classList.remove('show');
+function generateResume() {
+    html2pdf(areaCv, opt);
 }
 
-navLink.forEach(n => n.addEventListener('click', linkAction));
+resumeButton.addEventListener("click", () => {
+    scaleCV();
 
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '80px',
-    duration: 1000,
-    reset: false
-});
+    generateResume();
 
-sr.reveal('.home_title', {});
-sr.reveal('.button', { delay: 200 });
-sr.reveal('.home_img', { delay: 400 });
-sr.reveal('.home_social-icon', { interval: 200 });
-
-sr.reveal('.about_img', {});
-sr.reveal('.about_subtitle', { delay: 200 });
-sr.reveal('.about_text', { delay: 400 });
-
-sr.reveal('.skills_subtitle', {});
-sr.reveal('.skills_text', { delay: 200 });
-sr.reveal('.skills_data', { interval: 100 });
-
-sr.reveal('.education_content', { delay: 200 });
-sr.reveal('.experience_content', { delay: 200 });
-sr.reveal('.project_content', { delay: 200 });
-
-sr.reveal('.footer_title', {});
-sr.reveal('.footer_social', { delay: 200 });
+    setTimeout(() => {
+        removeScale();
+    }, 2500);
+})
